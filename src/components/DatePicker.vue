c155d669ed92404765b7adc51de2133004437adb
michaellovisa <michael.lovisa@proxybus.com>
2018-12-17 11:52:09 -0500

    Wip refine inline design

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,5 +1,5 @@
 <template lang='pug'>
-  .datepicker__wrapper(v-if='show' v-on-click-outside='clickOutside' @blur="clickOutside" :class="`${openMode ? 'datepicker__wrapper--open-mode' : ''}` " :style="design === 'inline' ? '' : {background: 'none'}")
+  .datepicker__wrapper(v-if='show' v-on-click-outside='clickOutside' @blur="clickOutside" :class="`${openMode ? 'datepicker__wrapper--open-mode' : ''}`" :style="design === 'inline' ? {background: 'none'} : ''")
     .datepicker__close-button.-hide-on-desktop(v-if='isOpen && !openMode' @click='hideDatepicker') ＋
     .datepicker__dummy-wrapper(  :class="`${isOpen ? 'datepicker__dummy-wrapper--is-active' : ''} ${openMode ? 'datepicker__dummy-wrapper--open-mode' : ''}`" :style="design === 'boxed' ? boxedStyle : {border: 'none'}")
       date-input(
